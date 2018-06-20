455b353ec0848428771dc1602a3d5b2c8ef5d291
aakashrajput116 <aakashrajput116@gmail.com>
2018-06-20 06:35:43 +0530

    First Program

---
+++ 2.php
@@ -0,0 +1,27 @@
+<?php
+$name="Aakash";
+$age=20;
+$address="vkp";
+$salary=123.52;
+$subjects=array("Ds",2,"C",3,"Python",5,"7");
+$status=true;
+
+echo $age;
+echo"<br>";
+echo $address;
+echo"<br>";
+echo$salary;
+echo"<br>";
+print $name;
+echo"<br>";
+print_r($subjects);
+echo"<br>";
+echo $status;
+echo"<br/>";
+
+print $subjects[0];
+echo"<br/>";
+print $subjects[1];
+echo"<br/>";
+print $status;
+?>
---
+++ 3.php
@@ -0,0 +1,8 @@
+<?php
+$subjects=array("Ds",2,"C",3,"Python",5,"7");
+for($i=0;$i<7;$i++)
+{
+    print $subjects[$i];
+    echo"<br>";
+}
+?>
